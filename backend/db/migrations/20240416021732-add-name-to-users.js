d486b55c43f59628e6008431cd67dce4706da5a2
Chase Kliment <stepintoliquidd@gmail.com>
2024-04-16 19:23:16 -0600

    Update migration scripts to handle schema settings consistently.

--- backend/db/migrations/20240416021732-add-name-to-users.js
+++ backend/db/migrations/20240416021732-add-name-to-users.js
@@ -1,21 +1,29 @@
 'use strict';
 
+// Same approach to handling options as in create-user.js
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('Users', 'firstName', {
+    // Use tableName with options for specifying schema when modifying the table
+    await queryInterface.addColumn({ tableName: 'Users', schema: options.schema }, 'firstName', {
       type: Sequelize.STRING,
       allowNull: true
     });
-    await queryInterface.addColumn('Users', 'lastName', {
+    await queryInterface.addColumn({ tableName: 'Users', schema: options.schema }, 'lastName', {
       type: Sequelize.STRING,
       allowNull: true
     });
   },
 
-
-  async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Users', 'firstName');
-    await queryInterface.removeColumn('Users', 'lastName');
+  async down(queryInterface, Sequelize) {
+    // Use tableName with options for specifying schema when modifying the table
+    await queryInterface.removeColumn({ tableName: 'Users', schema: options.schema }, 'firstName');
+    await queryInterface.removeColumn({ tableName: 'Users', schema: options.schema }, 'lastName');
   }
 };
+
