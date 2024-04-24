c010f9078458b17bd71010d8a3b4c6ce63377b59
Chase Kliment <stepintoliquidd@gmail.com>
2024-04-23 19:01:23 -0600

    added a review-images.js

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,7 +2,10 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
-const spotsRouter = require('./spots.js')
+const spotsRouter = require('./spots.js');
+const reviewsRouter = require('./reviews.js');
+const spotImagesRouter = require('./spot-images.js');
+const reviewImagesRouter = require('./review-images.js');
 const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
@@ -13,6 +16,8 @@ router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
 router.use('/reviews', reviewsRouter); // Use the reviews router
+router.use('/spot-images', spotImagesRouter); // Use the spot-images router
+router.use('/review-images', reviewImagesRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
