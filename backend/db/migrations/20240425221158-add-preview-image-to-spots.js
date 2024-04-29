e6b19d73519e8e3ff8ad0d0965f122e15bd59a90
Chase Kliment <stepintoliquidd@gmail.com>
2024-04-29 16:12:28 -0600

    added logging to see where deployment failed

--- backend/db/migrations/20240425221158-add-preview-image-to-spots.js
+++ backend/db/migrations/20240425221158-add-preview-image-to-spots.js
@@ -5,18 +5,29 @@ if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;
 }
 
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn({ tableName: 'Spots', schema: options.schema }, 'previewImage', {
-      type: Sequelize.STRING,
-      allowNull: true,
-      defaultValue: null
-    });
+  async up(queryInterface, Sequelize) {
+    try {
+      console.log("Adding previewImage column to Spots table...");
+      await queryInterface.addColumn({ tableName: 'Spots', schema: options.schema }, 'previewImage', {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null
+      });
+      console.log("Column added.");
+    } catch (error) {
+      console.error("Error adding column:", error);
+    }
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn({ tableName: 'Spots', schema: options.schema }, 'previewImage');
+  async down(queryInterface, Sequelize) {
+    try {
+      console.log("Removing previewImage column from Spots table...");
+      await queryInterface.removeColumn({ tableName: 'Spots', schema: options.schema }, 'previewImage');
+      console.log("Column removed.");
+    } catch (error) {
+      console.error("Error removing column:", error);
+    }
   }
 };
