614165ebbf0d7856c823c9444cc14d2c284f483c
Chase Kliment <stepintoliquidd@gmail.com>
2024-06-01 17:47:26 -0600

    commingiting auth me code completed

--- frontend/src/components/Navigation/OpenModalMenuItem.jsx
+++ frontend/src/components/Navigation/OpenModalMenuItem.jsx
@@ -1,4 +1,5 @@
 import { useModal } from '../../context/Modal';
+import './Navigation.css'
 
 function OpenModalMenuItem({
   modalComponent, // component to render inside the modal
@@ -15,7 +16,7 @@ function OpenModalMenuItem({
   };
 
   return (
-    <li onClick={onClick}>{itemText}</li>
+    <li className= 'modal-link' onClick={onClick}>{itemText}</li>
   );
 }
 
